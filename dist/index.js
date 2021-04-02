d8812996d2a7f41f4d09fa05ef1d557bdced0458
Daniel Sales Álvarez <dsa21may@gmail.com>
2021-04-03 01:18:11 +0200

    .

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
-
-console.log("npx npgen");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+const Prompt_1 = require("./src/prompt/Prompt");
+(function main() {
+    return tslib_1.__awaiter(this, void 0, void 0, function* () {
+        yield Prompt_1.showPrompt();
+    });
+}());
+//# sourceMappingURL=index.js.map
