582e5f430accae381288baf50c598da058973397
Daniel Sales Álvarez <dsa21may@gmail.com>
2021-04-02 21:23:22 +0200

    chore: add env node

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,14 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+#!/usr/bin/env node
+
+Object.defineProperty(exports, "__esModule", {
+  value: true,
+} );
 const tslib_1 = require("tslib");
 const Prompt_1 = require("./src/prompt/Prompt");
+
 (function main() {
-    return tslib_1.__awaiter(this, void 0, void 0, function* () {
-        yield Prompt_1.showPrompt();
-    });
+  return tslib_1.__awaiter(this, void 0, void 0, function* () {
+    yield Prompt_1.showPrompt();
+  } );
 }());
-//# sourceMappingURL=index.js.map
+// # sourceMappingURL=index.js.map
